b34e40e052ee78334e85e24b8086612c0825ceb9
samikhan <smaikhan4475@gmail.com>
2024-11-16 01:02:52 +0500

    design fix

--- src/components/molecules/profile-card.tsx
+++ src/components/molecules/profile-card.tsx
@@ -16,18 +16,18 @@ const ProfileCard = ({
   tooltipTitle,
 }: ProfileCardProps) => {
   return (
-    <Flex direction="col" align="center" gap={3}>
+    <Flex direction="col" align="center" justify="center" gap={3}>
       <Tooltip
         title={
-          <Flex className="min-w-max gap-2.5  items-center">
+          <Flex className="min-w-max gap-2.5   items-center">
             <TooltipIcon /> {tooltipTitle}
           </Flex>
         }
-        className="text-teal-500  "
+        className="text-teal-500"
       />
       <Flex
         className={
-          "bg-white  rounded-xl shadow-lg p-9 max-sm:p-3 max-sm:gap-4 gap-9"
+          "bg-white rounded-xl shadow-lg p-9 max-sm:p-9 max-sm:!w-[234px] max-sm:gap-4 gap-9"
         }
         direction="col"
       >
